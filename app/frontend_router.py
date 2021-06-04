59df5f28519dd62bcdeacd65247f0f1af6d94d2d
polyonalr <yapzhhg@gmail.com>
2021-06-04 14:41:54 +0800

    Add frontend route to show all reports

--- app/frontend_router.py
+++ app/frontend_router.py
@@ -1,11 +1,29 @@
-from flask import Blueprint, request, abort, render_template
+from flask import Blueprint, request, abort, render_template, redirect, url_for
 from app.models import Report, HashResult
 import os
 import shutil
 import tempfile
-import weakref
+import math
 
 frontend_blueprint = Blueprint('frontend_router', __name__)
+
+@frontend_blueprint.route("/")
+def index():
+    return redirect(url_for("frontend_router.ShowAllReports"))
+
+@frontend_blueprint.route("/app/report", methods=['GET'])
+def ShowAllReports():
+    page_no = request.args.get('page')
+    if page_no == None:
+        page_no = 1
+    report_query = Report.query.order_by(Report.creation_datetime.desc()).paginate(page_no, 10, False)
+    return render_template(
+        'show_all_reports.html',
+        report_items = report_query.items,
+        total = report_query.total,
+        page_no = page_no,
+        max_page = math.ceil(report_query.total/10)
+    )
 
 @frontend_blueprint.route('/app/report/<report_name>', methods=['GET'])
 def GetReport(report_name='unspecified'):
